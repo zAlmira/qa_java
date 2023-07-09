d50ac519e91642a245d0f5e04819fe173b41e32d
Almira Zamaldinova <almirazamaldinova@yandex.ru>
2023-07-09 09:35:15 +0300

    commit

--- src/main/java/com/example/Animal.java
+++ src/main/java/com/example/Animal.java
@@ -2,7 +2,9 @@ package com.example;
 
 import java.util.List;
 
-public class Animal {
+public class Animal  {
+
+
 
     public List<String> getFood(String animalKind) throws Exception {
         if ("Травоядное".equals(animalKind)) {
@@ -14,7 +16,7 @@ public class Animal {
         }
     }
 
-    public String getFamily() {
+   public String getFamily() {
         return "Существует несколько семейств: заячьи, беличьи, мышиные, кошачьи, псовые, медвежьи, куньи";
     }
 }
